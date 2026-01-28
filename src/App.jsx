f5107f8d6af664acc7a72d3d44f53bfb6a5f6e61
sameer07-bit <shaiksameerahmed95112@gmail.com>
2026-01-28 08:37:38 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,17 @@ export default class App extends Component {
     super();
     this.state = {
       courses: [
-        { name: "Pokémon", duration: "2hrs", price: "Rs 100", img: "1.jpg" },
-        { name: "Doraemon",     duration: "2hrs", price: "Rs 100", img: "2.jpg" },
+        { name: "Pokémon", duration: "2hrs", price: "Rs 105", img: "1.jpg" },
+        { name: "Doraemon",     duration: "2hrs", price: "Rs 90", img: "2.jpg" },
         { name: "Toy Story 3",        duration: "2hrs", price: "Rs 100", img: "3.jpg" },
-        { name: "Car's 3 ",    duration: "2hrs", price: "Rs 100", img: "4.jpeg" },
+        { name: "Cars 3 ",    duration: "2hrs", price: "Rs 140", img: "4.jpeg" },
+         { name: "Frozen", duration: "2hrs", price: "Rs 120", img: "5.jpg" },
+    { name: "Kung Fu Panda", duration: "1hr 45min", price: "Rs 110", img: "6.jpg" },
+    { name: "Minions", duration: "1hr 50min", price: "Rs 105", img: "7.jpg" },
+    { name: "Baby Boss", duration: "1hr 37min", price: "Rs 115", img: "8.jpg" },
+    { name: "Incredibles", duration: "1hr 55min", price: "Rs 130", img: "9.jpeg" },
+    { name: "Moana", duration: "1hr 47min", price: "Rs 125", img: "10.jpeg" },
+    { name: "Beyblade", duration: "1hr 30min", price: "Rs 100", img: "11.jpg" },
       ]
     };
   }
