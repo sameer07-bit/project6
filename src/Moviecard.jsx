3e8ca5e114dc83f21a404cf55680f2a1ee125326
sameer07-bit <shaiksameerahmed95112@gmail.com>
2026-01-28 08:15:13 +0530

    initial commit

--- src/Moviecard.jsx
+++ src/Moviecard.jsx
@@ -1,37 +1,23 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import './App.css';
 class MovieCard extends Component {
-  render() {
-    const { data } = this.props;
-    const BASEURL = import.meta.env.BASE_URL || '/';
-
-    return (
-      <div className="movie-card">
-        <div className="card-image-wrapper">
-          <img 
-            src={BASEURL + data.img} 
-            alt={data.name} 
-            className="movie-poster"
-          />
-          <div className="image-overlay" />
-        </div>
-
-        <div className="card-content">
-          <h3 className="movie-title">{data.name}</h3>
-
-          <div className="info-row">
-            <span className="label">Duration</span>
-            <span className="value">{data.duration}</span>
-          </div>
-
-          <div className="info-row price-row">
-            <span className="label">Price</span>
-            <span className="value price">{data.price}</span>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const {data} = this.props;
+        const BASEURL = import.meta.env.BASE_URL;
+        return (
+            <div className='Moviecard'>
+                <img src={BASEURL + data.img} alt='' />
+                <h3>{data.name}</h3>
+                <p>
+                    <span>Duration</span>
+                    <span>{data.duration}</span>
+                </p>
+                <p>
+                    <span>Price</span>
+                    <span>{data.price}</span>
+                </p>
+            </div>
+        );
+    }
 }
-
 export default MovieCard;
